7453fc11681e83d10212b4d304b7f22337d41cf4
晋晨曦 <Jin0714@outlook.com>
2024-04-25 00:20:36 +0800

    feat(main.go): add main of SEP

---
+++ cmd/SEP/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"SEP/internal/configs"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	configs.InitLog()
+	configs.InitViper()
+	configs.InitDB()
+	configs.GetRouterConfig(e)
+	configs.PostRouterConfig(e)
+	configs.PutRouterConfig(e)
+	configs.DeleteRouterConfig(e)
+}
